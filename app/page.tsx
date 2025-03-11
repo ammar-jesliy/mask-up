33b7c6b61558e5097aac91eaa46754dc86019acf
ammar-jesliy <ammarjc1@gmail.com>
2025-03-11 20:45:45 +0530

    feat: add logo and change font to mono

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
-import ImageEditor from '../components/ImageEditor';
+import ImageEditor from "../components/ImageEditor";
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-4 md:p-8 bg-gray-50">
-      <div className="max-w-6xl mx-auto" >
+    <main className="min-h-screen p-4 md:p-8 bg-gray-50 font-mono">
+      <div className="max-w-6xl mx-auto">
         <div className="mb-8 text-center">
-          <h1 className="text-3xl md:text-4xl font-bold mb-2" >MaskUp</h1>
-          <p className="text-gray-600">A background effect generator tool</p>
+          <div className="flex items-center justify-center mb-4">
+            <div className="flex items-center gap-5">
+              <img src="/MaskUpLogo.png" alt="" className="h-15" />
+              <h1 className="text-3xl md:text-4xl font-bold">
+                MaskUp
+              </h1>
+            </div>
+          </div>
+          <p className="text-gray-600">
+            A background effect generator tool
+          </p>
         </div>
         <ImageEditor />
       </div>
