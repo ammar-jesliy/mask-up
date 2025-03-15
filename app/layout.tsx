b5eaf4f3867332ce9e0fd6fa5fd34210df1e5e68
ammar-jesliy <ammarjc1@gmail.com>
2025-03-15 17:45:11 +0530

    feat: add Open Graph and Twitter metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,34 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "MaskUp",
   description: "A background effect generator tool",
+  openGraph: {
+    title: "MaskUp",
+    description: "A background effect generator tool",
+    type: "website",
+    locale: "en_US",
+    url: "https://mask-up.vercel.app",
+    images: [
+      {
+        url: "https://mask-up.vercel.app/maskup-og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "MaskUp",
+      },
+    ],
+  },
+  twitter: {
+    site: "https://mask-up.vercel.app",
+    card: "summary_large_image",
+    creator: "@ammarjesliy",
+    images: [
+      {
+        url: "https://mask-up.vercel.app/maskup-og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "MaskUp",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
